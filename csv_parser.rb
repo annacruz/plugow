dee7b70b2c2a87cf7ada8064a39b05cabdbba41e
Anna <anna.cruz@gmail.com>
2012-11-30 16:24:21 -0200

    Improving converter

--- csv_parser.rb
+++ csv_parser.rb
@@ -1,17 +1,21 @@
 require 'csv'
 class CsvParser
 #Hash[*keys.zip(data).flatten]
-  def self.read!(filename)
+  def self.read! filename
     elements_array = []
     CSV.foreach(filename) do |row|
-      elements_array.push(row)
+      elements_array.push row
     end
     return elements_array
   end
 
-  def self.to_json(elements_array)
+  def self.to_json elements_array
     keys = ["name", "email"]
-    elements_array.shift
-    Hash[*keys.zip(elements_array).flatten]
+    final_array = []
+    elements_array.each do |element|
+      element.shift(1)
+      final_array.push Hash[*keys.zip(element).flatten]
+    end
+    return final_array
   end
 end
--- spec/csv_parser_spec.rb
+++ spec/csv_parser_spec.rb
@@ -1,8 +1,9 @@
 require 'spec_helper'
 require './csv_parser'
 describe 'CsvParser' do
-  let!(:data) { [["123", " John Doe", " john@example.org"], ["8918", " Mary Doe", " mary@example.org"]]}
+  let!(:data) { [["123", "John Doe", "john@example.org"], ["8918", "Mary Doe", "mary@example.org"]]}
+  let!(:result) { [{"name" => "John Doe", "email" => "john@example.org"}, {"name" => "Mary Doe", "email" => "mary@example.org"}] }
   it "should return a hash after import csv" do
-    expect(CsvParser.to_json(data)).to be_a(Hash)
+    expect(CsvParser.to_json(data)).to be_eql(result)
   end
 end
